4e395bb2f3b153b584bd2ff839c2ea4a8805bed5
geoff-sheldon <geoffpeanutbutter@gmail.com>
2021-05-20 12:20:52 -0700

    Switch to mkpasswd

--- add-user.sh
+++ add-user.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 user=$1
-htpasswd -B auth/.htpasswd "$1"
+
+password=$(mkpasswd -m sha-512)
+
+#usename/password string
+upstring="$user:$password"
+
+echo $upstring >> auth/.passwords
